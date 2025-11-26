4afafab3c44f3895eb99c3b46d24760bc397a79c
Karel Jašek <karel.jasek@saboit.de>
2025-11-26 22:12:01 +0100

    fix build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -79,6 +79,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations("sqlite:data.db", migrations)
